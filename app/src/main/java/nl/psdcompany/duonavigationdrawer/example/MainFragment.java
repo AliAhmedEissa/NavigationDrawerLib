a94c9cb18aaef794757cf60105322022e0a2f420
AliEissa <alieasa806@gmail.com>
2020-10-16 20:35:54 +0200

    First Release

---
+++ app/src/main/java/nl/psdcompany/duonavigationdrawer/example/MainFragment.java
@@ -0,0 +1,39 @@
+package nl.psdcompany.duonavigationdrawer.example;
+
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Clicked a button!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+
+
+    }
+
